b1b6c1a13862c594283df829d54aed248ba34714
Pritish Puhan <54423565+pritishpuhan@users.noreply.github.com>
2019-10-04 19:21:08 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -2,7 +2,7 @@
 Data <- read.csv("C:/Users/Pritish Puhan/Downloads/exdata_data_household_power_consumption/household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?")
 ## Subsetting as per requirement
 Subset <- subset(Data, Data$Date =="1/2/2007" | Data$Date == "2/2/2007")
-#str(subSetData)
+## str(Subset)
 datetime <- strptime(paste(Subset$Date, Subset$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 globalActivePower <- as.numeric(Subset$Global_active_power)
 subMetering1 <- as.numeric(Subset$Sub_metering_1)
