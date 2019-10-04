5d7aaaf27de1662d70a1b36c416bfab0c659f785
Pritish Puhan <54423565+pritishpuhan@users.noreply.github.com>
2019-10-04 18:59:28 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -2,9 +2,9 @@
 Data <- read.csv("C:/Users/Pritish Puhan/Downloads/exdata_data_household_power_consumption/household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?")
 ## Subsetting as per requirement
 Subset <- subset(Data, Data$Date =="1/2/2007" | Data$Date == "2/2/2007")
-## str(subSetData)
-datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-globalActivePower <- as.numeric(subSetData$Global_active_power)
+#str(subSetData)
+datetime <- strptime(paste(Subset$Date, Subset$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(Subset$Global_active_power)
 png("plot2.png", width=480, height=480)
 plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
